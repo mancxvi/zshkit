78d78d4a982e3b3ffffa77c3987f6aae4b20c430
William Oberndorfer <will.oberndorfer@gmail.com>
2015-03-09 10:30:11 -0400

    add shims to path

--- 01_path.zsh
+++ 01_path.zsh
@@ -1,4 +1,4 @@
-path=($HOME/.rbenv/bin $HOME/.rbenv/shims /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin $path /usr/local/mysql/bin/ /opt/git/bin /usr/local/bin  /usr/texbin /usr/sfw/bin /usr/ccs/bin /usr/sbin)
+path=($HOME/.rbenv/bin $HOME/.rbenv/shims /opt/local/bin $HOME/bin $HOME/.gem/ruby/1.8/bin $path /usr/local/mysql/bin /opt/git/bin /usr/local/bin  /usr/texbin /usr/sfw/bin /usr/ccs/bin /usr/sbin)
 
 # Allow MacPorts man pages
 manpath=($manpath /opt/local/man)
